8a558c4c8194e7dedc69c7529617ebeccf4b71c1
icecat <icecatm7@gmail.com>
2026-09-11 08:54:54 +0700

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,22 +5,16 @@ export ARCH=arm64
 export SUBARCH=arm64
 mkdir -p out
 
-CLANG_DIR=$(find $(pwd)/toolchain/clang-19 -maxdepth 2 -type d -name "bin" | head -n1 | xargs dirname)
+CLANG_DIR=$(find $(pwd)/toolchain/clang-19 -maxdepth 2 -type d -name "bin" -path "*r530567*" | head -n1 | xargs dirname)
 export PATH=$CLANG_DIR/bin:$PATH
 
 echo "Using clang from: $CLANG_DIR"
 $CLANG_DIR/bin/clang --version
 
-CLANG_TRIPLE=aarch64-linux-gnu-
-KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+make -C $(pwd) O=$(pwd)/out ARCH=arm64 vendor/a52sxq_eur_open_defconfig
 
-make -j$(nproc) -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV \
-    ARCH=arm64 LLVM=1 LLVM_IAS=1 CLANG_TRIPLE=$CLANG_TRIPLE \
-    CONFIG_SECTION_MISMATCH_WARN_ONLY=y \
-    vendor/a52sxq_eur_open_defconfig
-
-make -j$(nproc) -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV \
-    ARCH=arm64 LLVM=1 LLVM_IAS=1 CLANG_TRIPLE=$CLANG_TRIPLE \
+make -j$(nproc) -C $(pwd) O=$(pwd)/out \
+    ARCH=arm64 CC=clang LLVM=1 LLVM_IAS=1 CROSS_COMPILE=aarch64-linux-gnu- \
     CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
